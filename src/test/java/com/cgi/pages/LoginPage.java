beb9aec16d6809c0f414a950dc9391749715495f
Balaji <balaji0017@gmail.com>
2024-06-14 09:47:02 +0100

    page object added

--- src/test/java/com/cgi/pages/LoginPage.java
+++ src/test/java/com/cgi/pages/LoginPage.java
@@ -4,23 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
-	
+
 	private WebDriver driver;
-	
-	public LoginPage(WebDriver driver)
-	{
-		this.driver=driver;
+
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
 	}
-	
+
 	public void enterUsername(String username) {
 		driver.findElement(By.name("username")).sendKeys(username);
 	}
-	
+
 	public void enterPassword(String password) {
 		driver.findElement(By.name("password")).sendKeys(password);
 	}
-	
-	//clickOnLogin()
-	
-	//getInvalidErrorMessage()
+
+	// clickOnLogin()
+	public void clickOnLogin() {
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+	}
+
+	// getInvalidErrorMessage()
+	public String getInvalidErrorMessage() {
+		return driver.findElement(By.xpath("//p[contains(normalize-space(),'Inval')]")).getText();
+	}
 }
--- src/test/java/com/cgi/test/LoginTest.java
+++ src/test/java/com/cgi/test/LoginTest.java
@@ -1,36 +1,34 @@
 package com.cgi.test;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.cgi.base.AutomationWrapper;
+import com.cgi.pages.DashboardPage;
 import com.cgi.pages.LoginPage;
 import com.cgi.utilities.DataUtils;
 
 public class LoginTest extends AutomationWrapper {
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class,groups = {"smoke","login"})
-	public void validLoginTest(String username,String password,String expectedValue) {
-		//driver.findElement(By.name("username")).sendKeys(username);
-		LoginPage login=new LoginPage(driver);
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class, groups = { "smoke", "login" })
+	public void validLoginTest(String username, String password, String expectedValue) {
+		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
-		
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		login.clickOnLogin();
 
-		String actualText = driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
+		DashboardPage dashboard = new DashboardPage(driver);
+		String actualText = dashboard.getQuickLaunchText();
 		Assert.assertEquals(actualText, expectedValue);
 	}
 
-	@Test(dataProvider = "commonDataProvider",dataProviderClass = DataUtils.class,groups = {"login"})
+	@Test(dataProvider = "commonDataProvider", dataProviderClass = DataUtils.class, groups = { "login" })
 	public void invalidLoginTest(String username, String password, String expectedError) {
-		LoginPage login=new LoginPage(driver);
+		LoginPage login = new LoginPage(driver);
 		login.enterUsername(username);
 		login.enterPassword(password);
-		
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		login.clickOnLogin();
 
-		String actualError = driver.findElement(By.xpath("//p[contains(normalize-space(),'Inval')]")).getText();
+		String actualError = login.getInvalidErrorMessage();
 		Assert.assertTrue(actualError.contains(expectedError)); // expect true
 
 	}
